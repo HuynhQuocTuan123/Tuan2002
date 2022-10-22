b58b2eae3c13c7d6adce23bbb85a03a743958de1
tuan <tuanhqgcc200311@fpt.edu.vn>
2022-10-22 16:32:41 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = pg_connect("postgres://cdrrsktpbfjwaj:d4c2d8e20c64df217eb3cfd68d25b1641f924bf2db54b84b53cdca94d24ede8c@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d9a7o0ldo4aahk");
+$conn = pg_connect("postgres://postgres://cdrrsktpbfjwaj:d4c2d8e20c64df217eb3cfd68d25b1641f924bf2db54b84b53cdca94d24ede8c@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d9a7o0ldo4aahk
+");
 	if(!$conn)
 	{
 		die("Could not connect to database");
