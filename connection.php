f3a186bcbbc848c6816dc0978cc13d243291e2a9
tuan <tuanhqgcc200311@fpt.edu.vn>
2022-10-24 13:42:54 +0700

    tuan

--- connection.php
+++ connection.php
@@ -1,7 +1,6 @@
 <?php
 
-$conn = pg_connect("postgres://oloymrxmishpid:b6c1b49d3d73f7f0a1042a8b912c267c18d380ee99fe0dacb3a67c8e4e05d990@ec2-54-82-205-3.compute-1.amazonaws.com:5432/ddbhoi81dkfn80
-");
+$conn = pg_connect("postgres://oloymrxmishpid:b6c1b49d3d73f7f0a1042a8b912c267c18d380ee99fe0dacb3a67c8e4e05d990@ec2-54-82-205-3.compute-1.amazonaws.com:5432/ddbhoi81dkfn80");
 	if(!$conn)
 	{
 		die("Could not connect to database");
